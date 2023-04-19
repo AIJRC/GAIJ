451ae6cf6f82d528f9e3677b1402ea61c2494799
Francisco Meirinhos <fcmeirinhos@gmail.com>
2023-04-19 14:34:36 +0200

    Added OCR pipeline

---
+++ ocr_pipeline.py
@@ -0,0 +1,58 @@
+import PIL
+import tesserocr
+
+import pandas as pd
+from io import StringIO
+
+import os
+import logging
+import queue
+import concurrent.futures
+
+
+def ocr_image(image_path, queue, preprocess=True):
+    api = None
+    try:
+        api = queue.get(block=True, timeout=600)
+
+        tsv = "level\tpage_num\tblock_num\tpar_num\tline_num\tword_num\tleft\ttop\twidth\theight\tconf\ttext\n"
+        with PIL.Image.open(image_path) as image:
+            for page in PIL.ImageSequence.Iterator(image):
+                api.SetImage(page)
+                data = api.GetTSVText(0)
+                tsv += data
+        return tsv
+    except queue.Empty:
+        return None
+    finally:
+        if api is not None:
+            queue.put(api)
+
+
+if __name__ == "__main__":
+    NUM_THREADS = 8
+
+    tesserocr_queue = queue.Queue(maxsize=NUM_THREADS)
+    for _ in range(NUM_THREADS):
+        api = tesserocr.PyTessBaseAPI(
+            lang="nor+eng", psm=6
+        )  # appears to be better at digit recognition than just 'nor'
+        api.SetVariable(
+            "preserve_interword_spaces", "1"
+        )  # optimal for extracting table-like data
+        tesserocr_queue.put(api)
+
+    # Find all .tif files
+    images = list(filter(lambda x: x.endswith(".tif"), os.listdir("./")))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+
+        def procedure(image_path):
+            tsv = ocr_image(image_path, tesserocr_queue)
+            try:
+                df = pd.read_table(StringIO(tsv), sep="\t", quoting=3)
+                df.to_csv(os.path.splitext(image_path)[0] + ".csv", index=False)
+            except Exception as e:
+                logging.warning("Failed writting {}".format(image_path), e)
+
+        out = executor.map(procedure, images)
